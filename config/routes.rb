c8be8e49f99278c2149c9257ced02ab99c311e63
kimchouard <kim.chouard@gmail.com>
2014-11-30 12:07:25 +0800

    Static pages controller and html imported

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static#index'
+  get '/about', to: 'static#about'
+  get '/contact', to: 'static#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
