3f83243a048fceda3d54dde3b1e29db1c9ade354
kimchouard <kim.chouard@gmail.com>
2014-12-09 10:54:20 +0000

    Automation of pat-get update, classic libs, git and oh-my-zsh (first commit from the vagrant box!)

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -11,41 +11,42 @@ echo "============= PERMISSIONS ============="
 chmod 600 $1/.ssh/id_rsa
 
 cd /tmp
-# sudo apt-get update
+echo "============= Update ============="
+sudo apt-get -qq update
 
 echo "============= MISCs ============="
-apt-get install -y -q libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+apt-get install -y -qq libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 
-# echo "============= GIT ============="
-# apt-get install -y -q git
+echo "============= GIT ============="
+apt-get install -y -qq git
 
-# echo "============= ZSH ============="
-# # Install zsh
-# apt-get install -y -q zsh
+echo "============= ZSH ============="
+# Install zsh
+apt-get install -y -qq zsh
 
-# # Clone oh-my-zsh
-# if [ ! -d ~/.oh-my-zsh ]; then
-#   git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-# fi
+# Clone oh-my-zsh
+if [ ! -d $1/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git $1/.oh-my-zsh
+fi
 
-# # Create a new zsh configuration from the provided template
-# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+# Create a new zsh configuration from the provided template
+cp $1/.oh-my-zsh/templates/zshrc.zsh-template $1/.zshrc
 
-# # Customize theme
-# sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="bira"/' ~/.zshrc
+# Customize theme
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="bira"/' $1/.zshrc
 
-# # add aliases
-# sed -i -e 's/# Example aliases/source ~\/.bash_aliases/gi' ~/.zshrc
+# add aliases
+sed -i -e 's/# Example aliases/source ~\/.bash_aliases/gi' $1/.zshrc
 
-# # Set zsh as default shell
-# chsh -s /bin/zsh vagrant
+# Set zsh as default shell
+chsh -s /bin/zsh vagrant
 
-# chown -R vagrant:vagrant ~/.oh-my-zsh
+chown -R vagrant:vagrant $1/.oh-my-zsh
 
 # echo "============= RUBY (via RVM) ============="
 # curl -sSL https://get.rvm.io | bash -s stable
 # source /usr/local/rvm/scripts/rvm
-# echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
+# echo "source $1/.rvm/scripts/rvm" >> $1/.bashrc
 
 # echo "============= NODE ============="
 # sudo add-apt-repository ppa:chris-lea/node.js
