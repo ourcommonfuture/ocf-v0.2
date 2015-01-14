862acd3173b93242bb6003046501198cb1d7547c
JonathanTeyssandier <jonathan.teyssandier@gmail.com>
2015-01-14 00:26:36 +0000

    Created blog page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static#about'
   get '/contact', to: 'static#contact'
   get '/mission', to: 'static#mission'
+  get '/blog', to: 'static#blog'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
