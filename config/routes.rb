f815f4eeda76cea38a8534579edfd475bfaf72e2
JonathanTeyssandier <jonathan.teyssandier@gmail.com>
2015-01-14 20:10:58 +0000

    Removed Contact link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static#index'
   get '/about', to: 'static#about'
-  get '/contact', to: 'static#contact'
+  # get '/contact', to: 'static#contact'
   get '/mission', to: 'static#mission'
   get '/blog', to: 'static#blog'
 
