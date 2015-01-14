619c868d91169b0424c3537d012928dc049d10c6
JonathanTeyssandier <jonathan.teyssandier@gmail.com>
2015-01-14 00:47:22 +0000

    FB like button implementation

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -25,6 +25,15 @@ jQuery(window).scroll(function () {
   }
 });
 
+// FACEBOOK BUTTON
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));	
+
 //----NICE SCROLL---//  
 jQuery(document).ready(function($){
     $("html").niceScroll({
